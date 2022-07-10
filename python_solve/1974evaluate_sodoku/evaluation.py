194577b148ba5014691890ac592930e7e112aa3a
ManduTheCat <krocd@naver.com>
2022-07-11 04:43:03 +0900

    column check 함수 작성

--- python_solve/1974evaluate_sodoku/evaluation.py
+++ python_solve/1974evaluate_sodoku/evaluation.py
@@ -1,11 +1,25 @@
 
+def check_col_direction(sodoku):
+  check_map = {n:0 for n in range(1,10)}
+
+  for fix_index in range(9):
+    check_map = {n:0 for n in range(1,10)}
+    for move_index in range(9):
+      check_map[sodoku[move_index][fix_index]]+=1
+    for k, v in check_map.items():
+      if v == 0:
+        return False
+  return True
+
+
 def main():
   input_count = int(input())
   for i in range(input_count):
     sodoku = []
-    print(i)
+    #print(i)
     for _ in range(9):
       sodoku.append(list(map(int, input().split())))
-    print(*sodoku, sep='\n')
+    #print(*sodoku, sep='\n')
+    check_col_direction(sodoku)
 if __name__ == "__main__":
   main()
