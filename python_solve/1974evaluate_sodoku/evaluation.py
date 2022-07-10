78f35d91dc7e8e3ca6a7e080715041f913466750
ManduTheCat <krocd@naver.com>
2022-07-11 05:37:36 +0900

    수도쿠 사각형 검사 추가

--- python_solve/1974evaluate_sodoku/evaluation.py
+++ python_solve/1974evaluate_sodoku/evaluation.py
@@ -1,22 +1,34 @@
+def check_square(sodoku: list):
+  check_map = {n: 0 for n in range(1,10)}
+  for start_row_index in range(0,9,3):
+    for start_col_index  in range(0,9,3):
+      check_map = {n: 0 for n in range(1,10)}
+      for row in sodoku[start_row_index: start_row_index +3]:
+        #print(row[start_col_index: start_col_index +3])
+        for el in row[start_col_index: start_col_index +3]:
+          check_map[el] +=1
+      if not evaluation_map(check_map):
+        return False
+  return True
 
 def check_col_direction(sodoku: list):
-  check_map = {n:0 for n in range(1,10)}
+  check_map = {n: 0 for n in range(1,10)}
   for fix_index in range(9):
-    check_map = {n:0 for n in range(1,10)}
+    check_map = {n: 0 for n in range(1,10)}
     for move_index in range(9):
       check_map[sodoku[move_index][fix_index]]+=1
     if not evaluation_map(check_map):
-      return evaluation_map(check_map)
+      return False
   return True
 
 def check_row_direction(sodoku: list):
-  check_map = {n:0 for n in range(1,10)}
+  check_map = {n: 0 for n in range(1,10)}
   for row in sodoku:
-    check_map = {n:0 for n in range(1,10)}
+    check_map = {n: 0 for n in range(1,10)}
     for el in row:
       check_map[el]+=1
     if not evaluation_map(check_map):
-      return evaluation_map(check_map)
+      return False
   return True
 
 def evaluation_map(map: dict):
@@ -33,6 +45,10 @@ def main():
     for _ in range(9):
       sodoku.append(list(map(int, input().split())))
     #print(*sodoku, sep='\n')
-    print(check_row_direction(sodoku), check_col_direction(sodoku))
+    if(check_row_direction(sodoku) and check_col_direction(sodoku) and check_square(sodoku)):
+      print(f'#{i+1} {1}')
+    else:
+      print(f'#{i+1} {0}')
+
 if __name__ == "__main__":
   main()
