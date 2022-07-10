7efa3006ec2cb1f10db695e9509e67cc33126600
ManduTheCat <krocd@naver.com>
2022-07-11 04:55:26 +0900

    row 에서 검사 추가

--- python_solve/1974evaluate_sodoku/evaluation.py
+++ python_solve/1974evaluate_sodoku/evaluation.py
@@ -1,16 +1,29 @@
 
-def check_col_direction(sodoku):
+def check_col_direction(sodoku: list):
   check_map = {n:0 for n in range(1,10)}
-
   for fix_index in range(9):
     check_map = {n:0 for n in range(1,10)}
     for move_index in range(9):
       check_map[sodoku[move_index][fix_index]]+=1
-    for k, v in check_map.items():
-      if v == 0:
-        return False
+    if not evaluation_map(check_map):
+      return evaluation_map(check_map)
   return True
 
+def check_row_direction(sodoku: list):
+  check_map = {n:0 for n in range(1,10)}
+  for row in sodoku:
+    check_map = {n:0 for n in range(1,10)}
+    for el in row:
+      check_map[el]+=1
+    if not evaluation_map(check_map):
+      return evaluation_map(check_map)
+  return True
+
+def evaluation_map(map: dict):
+  for k, v in map.items():
+    if v == 0:
+      return False
+  return True
 
 def main():
   input_count = int(input())
@@ -20,6 +33,6 @@ def main():
     for _ in range(9):
       sodoku.append(list(map(int, input().split())))
     #print(*sodoku, sep='\n')
-    check_col_direction(sodoku)
+    print(check_row_direction(sodoku), check_col_direction(sodoku))
 if __name__ == "__main__":
   main()
