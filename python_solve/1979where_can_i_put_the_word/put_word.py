93844259915f01daa8c9fb71e1c53d35cee861ca
[서울/2]김명진 <krocd923@gmail.com>
2022-07-10 18:53:32 +0900

    위에서 아래로 봐서 패턴 찾는 함수 작성

--- python_solve/1979where_can_i_put_the_word/put_word.py
+++ python_solve/1979where_can_i_put_the_word/put_word.py
@@ -1,4 +1,4 @@
-def check_len(K, puzzle):
+def see_left(K, puzzle):
   pattern_count = 0
   for row in puzzle:
     count = 0
@@ -14,8 +14,6 @@ def check_len(K, puzzle):
         if count_k( count, K):
           pattern_count+=1
         count = 0
-    #print(row, "el: ",el,"index:", index, "count:", count, "res:", res)
-  print("Res: ",pattern_count)
   return pattern_count
 
 def count_k( count, K):
@@ -24,20 +22,38 @@ def count_k( count, K):
     else:
        return False
 
+def see_top(K, puzzle):
+  pattern_count = 0
+  count = 0
+  N  = len(puzzle)
+  for fix_index in range(N):
+    count = 0
+    for move_index in range(N):
+      if move_index == N -1:
+        if puzzle[move_index][fix_index] == 1:
+          count+=1
+        if count_k(count, K):
+          pattern_count+=1
+      elif puzzle[move_index][fix_index] == 1:
+        count+=1
+      elif puzzle[move_index][fix_index] == 0:
+        if count_k(count, K):
+          pattern_count+=1
+        count = 0
+  return pattern_count
+
 def main():
   input_count = int(input())
   for i in range(input_count):
-    N, K = map(int, input().split(" "))
+    N, K = map(int, input().split())
     puzzle = []
-    print(i+1," 번째","  패턴 1 갯수: ", K)
     for _ in range(N):
-      row = list(map(int,input().split(" ")))
+      row = list(map(int,input().split()))
       puzzle.append(row)
-      print(row)
-    check_len(K, puzzle)
-    print("~~~~~~~~~~~~~~~~~~")
+    left = see_left(K, puzzle)
+    top = see_top(K, puzzle)
+    print(f'#{i+1} {left+top}')
 
-## 만약 정규식으로 한다면?
 
 if __name__ == "__main__":
   main()
