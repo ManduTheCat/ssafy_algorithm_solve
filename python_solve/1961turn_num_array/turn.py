59287f6810787339057ec4f518ca2dc282ee4801
ManduTheCat <krocd@naver.com>
2022-07-12 03:08:56 +0900

    출력양식 맞게 출력하는 print_res 함수 작성

--- python_solve/1961turn_num_array/turn.py
+++ python_solve/1961turn_num_array/turn.py
@@ -6,21 +6,31 @@ def turn_othogonal(N, arr: list):
   for fix_index in range(N):
     for move_index in range(N):
       result[fix_index].appendleft(arr[move_index][fix_index])
-  #print(*result, sep="\n")
   return result
+
+def print_res(N, res: list):
+  for i in range(N):
+    print()
+    for j in range(N):
+      print(*res[j][i], sep="", end=" ")
 
 def main():
   input_count = int(input())
   for i in range(input_count):
     arr = []
+    res = []
     N = int(input())
     for _ in range(N):
       arr.append(list(map(int, sys.stdin.readline().strip().split())))
-    #print(*arr, sep="\n")
-    print(i)
+    print(f"#{i+1}", end="")
     first = turn_othogonal(N, arr)
     second = turn_othogonal(N, first)
     third = turn_othogonal(N, second)
+    res.append(first)
+    res.append(second)
+    res.append(third)
+
+    print_res(N, res)
 
 if __name__ == "__main__":
   main()
