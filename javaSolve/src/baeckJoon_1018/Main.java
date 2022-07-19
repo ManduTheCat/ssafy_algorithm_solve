7973fa0ff3ed8f821b6be12b06307fe87bd07109
ManduTheCat <krocd@naver.com>
2022-07-20 00:11:50 +0900

    stram test

---
+++ javaSolve/src/baeckJoon_1018/Main.java
@@ -0,0 +1,14 @@
+package baeckJoon_1018;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int len =Integer.parseInt(bf.readLine());
+        int [] inputArr = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(inputArr);
+        System.out.printf("%d %d", inputArr[0], inputArr[len-1]);
+    }
+}
