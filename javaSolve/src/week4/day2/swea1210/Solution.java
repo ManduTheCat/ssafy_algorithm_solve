98bc2861d5481ba4ebbf789b831934dd7afbaf84
ManduTheCat <krocd@naver.com>
2022-08-02 16:28:19 +0900

    경로 변경 및 주석추가

--- javaSolve/src/week4/day2/swea1210/Solution.java
+++ javaSolve/src/week4/day2/swea1210/Solution.java
@@ -1,4 +1,4 @@
-package swea1210;
+package week4.day2.swea1210;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
@@ -10,7 +10,7 @@ public class Solution {
     public static final int n = 100;
 
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("resources/swea1210/input.txt"));
+        //System.setIn(new FileInputStream("resources/swea1210/input.txt"));
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         int[][] map = new int[n][n];
@@ -19,23 +19,24 @@ public class Solution {
             int TCnum = Integer.parseInt(bf.readLine());
             for (int i = 0; i < n; i++) {
                 map[i] = Arrays.stream(bf.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                //for (int j = 0; j < n; j++) {
-                //if(map[i][j] == 1){
-                //}
-                //}
             }
             System.out.printf("#%d ", TCnum);
             for (int i = 0; i < n; i++) {
                 if (map[0][i] == 1) {
-                    //System.out.printf(" 들어가는 위치  0, %d\n", i);
                     boolean[][] check = new boolean[n][n];
                     move(map, 0, i, check);
                 }
             }
         }
-        //System.out.println(Arrays.deepToString(check));
     }
 
+    /**
+     * 사다리 이동하면서 목적지에 닿으면 리턴하는 함수
+     * @param map 사다리 지도
+     * @param startR 입력받은 출발 row
+     * @param startC 입력받은 출발 column
+     * @param check 방문 체크 함수
+     */
     public static void move(int[][] map, int startR, int startC, boolean[][] check) {
         int[] dx = new int[]{-1, 1, 0};
         int[] dy = new int[]{0, 0, 1};
@@ -45,15 +46,8 @@ public class Solution {
         // 출발 좌표에서 이동
         while (startR <= 99) {
             for (int di = 0; di < 3; di++) {
-                //System.out.printf("%d,%d\n",dx[di], dy[di]);
                 if (checkBound(startR + dy[di], startC + dx[di])) {
-                    //System.out.println(di);
-                    // System.out.println(map[startR + dy[di]][startC + dx[di]]);
-                    //System.out.println(check[startR + dy[di]][startC + dx[di]] == true);
-                    //System.out.println(map[startR + dy[di]][startC + dx[di]] == 1);
-                    //System.out.printf("map %d, %d, %d\n", startR + dy[di],startC + dx[di], map[startR + dy[di]][startC + dx[di]]);
                     if ((map[startR + dy[di]][startC + dx[di]] == 1 || map[startR + dy[di]][startC + dx[di]] == 2) && !check[startR + dy[di]][startC + dx[di]]) {
-                        //System.out.println("in");
                         check[startR + dy[di]][startC + dx[di]] = true;
 
                         startR += dy[di];
@@ -62,9 +56,7 @@ public class Solution {
                     }
                 }
             }
-            //System.out.printf("%d, %d\n",startR,startC );
             if (map[startR][startC] == 2) {
-                //System.out.printf("정답이다!!!! %d, %d\n",resR, resC);
                 System.out.println(resC);
                 return;
             }
@@ -73,15 +65,15 @@ public class Solution {
             }
 
         }
-        // 이동중 범위 넘는지 체크
     }
 
+    /**
+     * 이동하면서 주어진 지도를 넘어가는치 확인하는 함수
+     * @param i 이동할 row
+     * @param j 이동할 column
+     * @return 벗어나면 false 맞으면 true
+     */
     public static boolean checkBound(int i, int j) {
-        if ((i >= n || i < 0 || j >= n || j < 0)) {
-            //System.out.printf("%d, %d\n", i, j);
-            //System.out.println("in");
-            return false;
-        }
-        return true;
+        return i < n && i >= 0 && j < n && j >= 0;
     }
 }
