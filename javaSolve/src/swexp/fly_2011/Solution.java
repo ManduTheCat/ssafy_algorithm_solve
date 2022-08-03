8f00f1792aa135cc34785cd070fc46481b7da6a0
김명진 <krocd@naver.com>
2022-08-04 00:53:09 +0900

    파리채 추후 다시

--- javaSolve/src/swexp/fly_2011/Solution.java
+++ javaSolve/src/swexp/fly_2011/Solution.java
@@ -12,24 +12,36 @@ public class Solution {
 
     public static void main(String [] argv)throws IOException {
         String path = Solution.class.getResource("").getPath();
-        System.setIn(new FileInputStream(path+"test1.txt"));
+        System.setIn(new FileInputStream(path+"test2.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int count_input = Integer.parseInt(br.readLine());
-        for(int i = 0; i< count_input; i++){
+        for(int tc = 0; tc< count_input; tc++){
             String[] NM = br.readLine().split(" ");
-            int n = Integer.parseInt(NM[0]);
-            int m = Integer.parseInt(NM[1]);
-            int [][] fild = new int[n][n];
-            for(int r = 0; r< n; r++){
+            int N = Integer.parseInt(NM[0]);
+            int M = Integer.parseInt(NM[1]);
+            int [][] fild = new int[N][N];
+            for(int r = 0; r< N; r++){
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int c = 0; c < n; c++){
+                for (int c = 0; c < N; c++){
                     fild[r][c] = Integer.parseInt(st.nextToken());
                 }
             }
-            System.out.println(Arrays.deepToString(fild));
+            int max = 0;
+            for (int i = 0; i < N - M + 1; i++) {
+                for (int j = 0; j < N - M + 1; j++) {
+                    int tmp = 0;
+                    for (int x = 0; x < M; x++) {
+                        for (int y = 0; y < M; y++) {
+                            tmp += fild[i + x][j + y];
+                        }
+                    }
+                    if (tmp > max) {
+                        max = tmp;
+                    }
+                }
+            }
+            System.out.printf("#%d %d\n", tc+1, max);
+
         }
-
-
-
     }
 }
