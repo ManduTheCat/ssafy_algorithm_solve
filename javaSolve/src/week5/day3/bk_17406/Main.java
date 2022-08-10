972c1d3aa222a2d87eb59c5a2fe7f089a229ae3b
ManduTheCat <krocd@naver.com>
2022-08-11 02:50:44 +0900

    배열돌리기3 회전 연산 완성

--- javaSolve/src/week5/day3/bk_17406/Main.java
+++ javaSolve/src/week5/day3/bk_17406/Main.java
@@ -14,7 +14,8 @@ public class Main {
     static int M;
     static int R;
     static int[][] map;
-    static ArrayList <Integer []> commandList;
+    static ArrayList<Integer[]> commandList;
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("resources/week5/day3/bk17406/input.txt"));
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -31,28 +32,84 @@ public class Main {
         }
         // map 인풋
         commandList = new ArrayList<>();
-        for(int command = 0;  command < R; command++){
+        for (int command = 0; command < R; command++) {
             // 3개 길이를 가지는 명령어 하나 할당
             StringTokenizer commandTk = new StringTokenizer(bf.readLine());
             commandList.add(new Integer[3]);
-            for(int index = 0; index < 3; index++){
+            for (int index = 0; index < 3; index++) {
                 commandList.get(command)[index] = Integer.parseInt(commandTk.nextToken());
             }
         }
-        printCommand();
 
+        //순열 돌면서 커맨드 실행
+        runCommand(commandList.get(0));
+
+    }
+
+    private static void runCommand(Integer[] command) {
+        int r = command[0]-1;
+        int c = command[1]-1;
+        int s = command[2];
+        startArray(r, c, s);
+    }
+
+    private static void startArray(int r, int c, int s) {
+        for (int ds = 1; ds <= s; ds++) {
+            int startR = r - ds;
+            int startC = c - ds;
+            int endR = r + ds;
+            int endC = c + ds;
+            System.out.printf("startR: %d startC: %d ds: %d endR: %d endC: %d\n", startR, startC, ds ,endR, endC);
+            turn(startR, startC, endR, endC);
+        }
+    }
+    private static void turn(int startR, int startC, int endR, int endC){
+        // 위쪽 오른쪽 방향으로 이동
+        // temp 로 맨 오른쪽이 짤리는걸 미리 저장 = 맨 오른쪽이 짤린다.
+        // 땡겨오는 느낌으로 교환
+        int upTemp = map[startR][endC];
+        for(int c = endC; c > startC; c-- ){
+            // column 이 오른쪽으로 이동
+            map[startR][c] = map[startR][c-1];
+        }
+
+        // 오른쪽 의 아래방향으로 이동
+        int leftTemp = map[endR][endC];
+        // row 방향으로 땡겨오는 느낌으로 교환
+        for(int r  = endR; r > startR+ 1; r--){
+            map[r][endC] = map[r-1][endC];
+        }
+        // 이전에 씹현던것은 여기서 넣어줘야한다.
+        map[startR+1][endC] = upTemp;
+
+        //아래 의 왼쪽 방향으로 이동
+        int downTemp = map[endR][startC];
+        for(int c = startC; c < endC-1; c++){
+            //System.out.print(map[endR][c]);
+            map[endR][c] = map[endR][c+ 1];
+        }
+        map[endR][endC-1] = leftTemp;
+
+        for(int r = startR; r< endR-1; r++){
+            System.out.print(map[r][startC]);
+            map[r][startC] = map[r+1][startC];
+        }
+        map[endR-1][startC] = downTemp;
+        System.out.println();
+
+        printMap();
 
     }
 
     private static void printCommand() {
-        for(Integer[] cmd : commandList){
+        for (Integer[] cmd : commandList) {
             System.out.println(Arrays.toString(cmd));
         }
 
     }
 
     public static void printMap() {
-        for (int[] m : map){
+        for (int[] m : map) {
             System.out.println(Arrays.toString(m));
         }
         System.out.println();
