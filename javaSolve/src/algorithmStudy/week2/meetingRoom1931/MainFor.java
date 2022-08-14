ce4662c75200b38a0f03f92901308352d9382db5
ManduTheCat <krocd@naver.com>
2022-08-14 21:44:03 +0900

    bk_1931 회의실 재귀 풀이 for 풀이

--- javaSolve/src/algorithmStudy/week2/meetingRoom1931/MainFor.java
+++ javaSolve/src/algorithmStudy/week2/meetingRoom1931/MainFor.java
@@ -4,48 +4,51 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-
-public class Main {
+public class MainFor {
     //회의의 시작시간과 끝나는 시간이 같을 수도 있다. 이 경우에는 시작하자마자 끝나는 것으로 생각하면 된다.
     //입력중에 같은 시간의회의가 존재할까?
     //
     static int n;
+
     static int max = 0;
     static int[][] roomList;
+
+
     public static void main(String[] args) throws IOException {
         //System.setIn(new FileInputStream("resources/study/week2/bk1931/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         roomList = new int[n][2];
         for (int i = 0; i < n; i++) {
-            int[] time = new int[2];
             StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] time = new int[2];
             time[0] = Integer.parseInt(st.nextToken());
             time[1] = Integer.parseInt(st.nextToken());
             roomList[i] = time;
         }
         Arrays.sort(roomList, (o1, o2) -> {
             if (o1[1] == o2[1]) {
-                return Integer.compare(o1[0], o2[0]);
+                return Integer.valueOf(o1[0]).compareTo(o2[0]);
             }
-            return Integer.compare(o1[1], o2[1]);
+            return Integer.valueOf(o1[1]).compareTo(o2[1]);
         });
+        //System.out.println(Arrays.deepToString(roomList));
+
+
         //그리디 ? - 활동 선택 문제
         // 시간이 가장 빨리 끝나는것 기준으로 탐색하면 ?
-
-        find(0, 0, 0);
-        System.out.println(max);
-    }
-
-    static void find(int curTime, int count, int index) {
-        max = Math.max(count, max);
-        for (int i = index; i < n; i++) {
-            if (curTime <= roomList[i][0]) {
-                find(roomList[i][1], ++count, i + 1);
-                break;
+        int depth = 0;
+        int currTime = 0;
+        for(int i = 0 ; i < roomList.length; i++){
+            if(currTime<= roomList[i][0] ){
+                currTime = roomList[i][1];
+                depth++;
             }
         }
+        System.out.println(depth);
     }
+
 }
