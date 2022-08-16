7a257d9873516b24a62960e3d3e271a1b7562e4c
ManduTheCat <krocd@naver.com>
2022-08-16 23:17:01 +0900

    JOL 1828 풀이

--- javaSolve/src/week6/day1/BJ1074/Main.java
+++ javaSolve/src/week6/day1/BJ1074/Main.java
@@ -10,29 +10,46 @@ public class Main {
     static int n;
     static int r;
     static int c;
-    static int [][]map;
     static int count;
+
     public static void main(String[] args) throws IOException {
-        System.setIn(new FileInputStream("resources/week6/BJ1074/input.txt"));
+        //System.setIn(new FileInputStream("resources/week6/BJ1074/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         r = Integer.parseInt(st.nextToken());
         c = Integer.parseInt(st.nextToken());
-        System.out.printf("%d, %d, %d", n, r, c);
-        map = new int[n][n];
+        //전체 길이는 2^n
+        int size = (int) Math.pow(2, n);
+        searchZ(0, 0, size);
 
     }
-    static void searchZ(int depth, int startR, int startC, int endR, int endC){
-        if(depth == n){
-            for(int i =  startR; i < endR; i++){
-                for(int j = startC; j < endC; j++){
-                    count++;
-                }
-            }
+
+    static void searchZ(int i, int j, int size) {
+        if (i == r && j == c) {
+            System.out.println(count);
             return;
         }
+        // 타겟 사분면에 존재하면 분할 재귀 시작
+        if (check(i, j, size)) {
+            //왼위
+            searchZ(i, j, size / 2);
+            // 오른위
+            searchZ(i, j + size / 2, size / 2);
+            // 왼아래
+            searchZ(i + size / 2, j, size / 2);
+            // 오른 아래
+            searchZ(i + size / 2, j + size / 2, size / 2);
 
+        } else count+= size * size; // 도만큼 인덱스 증가 한다.
 
+    }
+
+    // 현제 진입한 사분면 에 타겟이 존재하는지 체크
+    static boolean check(int i, int j, int size) {
+        if (r < i + size && i <= r && j <= c && c < j + size) {
+            return true;
+        }
+        return false;
     }
 }
