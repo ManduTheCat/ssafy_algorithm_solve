fd405a3202ac9a42ea5744de75d3e7331d3df2cc
김명진 <krocd@naver.com>
2022-08-29 10:02:21 +0900

    NM5 풀이

---
+++ javaSolve/src/dailySolve/BJ15654NM5/Main.java
@@ -0,0 +1,49 @@
+package dailySolve.BJ15654NM5;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] set;
+    static int[] res;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("resources/daily/BJ_15654/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        set = new int[N];
+        res = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(set);
+        permutation(0,0);
+        System.out.println(sb);
+
+    }
+    public static void permutation(int depth, int flag){
+        if(depth == M ){
+            for(int n: res){
+                sb.append(n).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N ; i++) {
+            if((flag &  1 <<i) == 0){
+                res[depth] = set[i];
+                permutation(depth+1, flag | 1 <<i);
+            }
+
+        }
+    }
+}
