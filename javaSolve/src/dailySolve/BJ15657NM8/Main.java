2aee9ebe4dcdc1cbe22c16f6fb900cd8129857f4
ManduTheCat <krocd@naver.com>
2022-09-01 23:18:29 +0900

    NM8 풀이

---
+++ javaSolve/src/dailySolve/BJ15657NM8/Main.java
@@ -0,0 +1,48 @@
+package dailySolve.BJ15657NM8;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] set;
+    static int[] res;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("resources/daily/BJ15657/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        set = new int[N];
+        res = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(set);
+        //System.out.println(Arrays.toString(set));
+        combination(0, 0);
+        System.out.println(sb);
+
+    }
+    public static void combination(int start, int depth){
+        if(depth == M){
+            for (int i = 0; i < M; i++) {
+                sb.append(res[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for(int i  = start; i < N; i++){
+            res[depth] = set[i];
+            combination(i, depth+1);
+        }
+    }
+}
