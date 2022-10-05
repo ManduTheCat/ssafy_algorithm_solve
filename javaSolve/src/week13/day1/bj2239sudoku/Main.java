5f57a1d0a54088b3e577740dc7b430ecd1806913
ManduTheCat <krocd@naver.com>
2022-10-05 09:04:32 +0900

    수도쿠 sol 등록

--- javaSolve/src/week13/day1/bj2239sudoku/Main.java
+++ javaSolve/src/week13/day1/bj2239sudoku/Main.java
@@ -1,82 +1,94 @@
 package week13.day1.bj2239sudoku;
 
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    // 안됨
-    static boolean[][] isPick = new boolean[9][10];
-    static int [][] map = new int[9][9];
-    //static int [] lineRes = new int[10];
+
+    public static int[][] arr = new int[9][9];
+
     public static void main(String[] args) throws IOException {
         System.setIn(new FileInputStream("resources/week13/day1/sudoku2239/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for (int row = 0; row < 9; row++) {
-            String inputRow = br.readLine();
-            for (int col = 0; col < 9; col++) {
-                int value = inputRow.charAt(col) - '0';
-                //System.out.println("vlaude " + value);
-                map[row][col] = value;
-                // 사용중인 숫자 true 체크
-                isPick[row][value] = true;
-                if(row == 0 && value == 9) System.out.println(isPick[row][9]);
-            }
-        }
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 10; j++) {
-                System.out.print(isPick[i][j] ? "1 ": "0 ");
-            }
-            System.out.println();
-        }
-        int [] lineRes = new int[10];
-        int count = 0;
-        Arrays.fill(lineRes, 0);
-        count = getZeroCount(0);
+        Scanner in = new Scanner(System.in);
 
-        permutation(0,0, count, lineRes);
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+
+        sudoku(0, 0);
+
     }
 
-    private static int getZeroCount(int r) {
-        int count = 0;
-        for (int i = 0; i < 9; i++) {
-            if(map[r][i]  == 0){
-                count++;
-            }
-        }
-        return count;
-    }
+    public static void sudoku(int row, int col) {
 
-    // 0일때 껴넣는 함수 필요함
-    private static void permutation( int r, int c, int count, int[] lineRes) {
-        if(r == 9){
-            System.out.println("in");
+        // 해당 행이 다 채워졌을 경우 다음 행의 첫 번째 열부터 시작
+        if (col == 9) {
+            sudoku(row + 1, 0);
             return;
         }
-        if(count == 0){
-            //System.out.println(Arrays.toString(lineRes));
-            //res 초기화
-            int [] newLineRes = new int[10];
-            if(r+1 < 9){
 
-                int zeroCount = getZeroCount(r+1);
-               // System.out.println(zeroCount);
-                permutation(r +1, 0,zeroCount, newLineRes);
+        // 행과 열이 모두 채워졌을 경우 출력 후 종료
+        if (row == 9) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
             }
-            // line 할당
-            // r 증가 c 0 해서 라인 할당 다시 돌린다 r== 9 가 될떄까지 재귀
+
+            // 출력 뒤 시스템을 종료한다.
+            System.exit(0);
+        }
+
+        // 만약 해당 위치의 값이 0 이라면 1부터 9까지 중 가능한 수 탐색
+        if (arr[row][col] == 0) {
+            for (int i = 1; i <= 9; i++) {
+                // i 값이 중복되지 않는지 검사
+                if (possibility(row, col, i)) {
+                    arr[row][col] = i;
+                    sudoku(row, col + 1);
+                }
+            }
+            arr[row][col] = 0;
             return;
         }
-        // count 갯수만큼만 뽑자
-        for (int i = 1; i < 10; i++) {
-            if(!isPick[r][i]){
-                isPick[r][i] = true;
-                lineRes[c] = i;
-                permutation(r,c+1, count-1 , lineRes );
-                isPick[r][i]= false;
+
+        sudoku(row, col + 1);
+
+    }
+
+    public static boolean possibility(int row, int col, int value) {
+
+        // 같은 행에 있는 원소들 중 겹치는 열 원소가 있는지 검사
+        for (int i = 0; i < 9; i++) {
+            if (arr[row][i] == value) {
+                return false;
             }
         }
+
+        // 같은 열에 있는 원소들 중 겹치는 행 원소가 있는지 검사
+        for (int i = 0; i < 9; i++) {
+            if (arr[i][col] == value) {
+                return false;
+            }
+        }
+
+        // 3*3 칸에 중복되는 원소가 있는지 검사
+        int set_row = (row / 3) * 3; // value가 속한 3x3의 행의 첫 위치
+        int set_col = (col / 3) * 3; // value가 속한 3x3의 열의 첫 위치
+
+        for (int i = set_row; i < set_row + 3; i++) {
+            for (int j = set_col; j < set_col + 3; j++) {
+                if (arr[i][j] == value) {
+                    return false;
+                }
+            }
+        }
+
+        return true; // 중복되는 것이 없을 경우 true 반환
     }
+
 }
---
+++ javaSolve/src/week13/day1/bj2239sudokuMe/Main.java
@@ -0,0 +1,82 @@
+package week13.day1.bj2239sudokuMe;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    // 안됨
+    static boolean[][] isPick = new boolean[9][10];
+    static int [][] map = new int[9][9];
+    //static int [] lineRes = new int[10];
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("resources/week13/day1/sudoku2239/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int row = 0; row < 9; row++) {
+            String inputRow = br.readLine();
+            for (int col = 0; col < 9; col++) {
+                int value = inputRow.charAt(col) - '0';
+                //System.out.println("vlaude " + value);
+                map[row][col] = value;
+                // 사용중인 숫자 true 체크
+                isPick[row][value] = true;
+                if(row == 0 && value == 9) System.out.println(isPick[row][9]);
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(isPick[i][j] ? "1 ": "0 ");
+            }
+            System.out.println();
+        }
+        int [] lineRes = new int[10];
+        int count = 0;
+        Arrays.fill(lineRes, 0);
+        count = getZeroCount(0);
+
+        permutation(0,0, count, lineRes);
+    }
+
+    private static int getZeroCount(int r) {
+        int count = 0;
+        for (int i = 0; i < 9; i++) {
+            if(map[r][i]  == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // 0일때 껴넣는 함수 필요함
+    private static void permutation( int r, int c, int count, int[] lineRes) {
+        if(r == 9){
+            System.out.println("in");
+            return;
+        }
+        if(count == 0){
+            //System.out.println(Arrays.toString(lineRes));
+            //res 초기화
+            int [] newLineRes = new int[10];
+            if(r+1 < 9){
+
+                int zeroCount = getZeroCount(r+1);
+               // System.out.println(zeroCount);
+                permutation(r +1, 0,zeroCount, newLineRes);
+            }
+            // line 할당
+            // r 증가 c 0 해서 라인 할당 다시 돌린다 r== 9 가 될떄까지 재귀
+            return;
+        }
+        // count 갯수만큼만 뽑자
+        for (int i = 1; i < 10; i++) {
+            if(!isPick[r][i]){
+                isPick[r][i] = true;
+                lineRes[c] = i;
+                permutation(r,c+1, count-1 , lineRes );
+                isPick[r][i]= false;
+            }
+        }
+    }
+}
