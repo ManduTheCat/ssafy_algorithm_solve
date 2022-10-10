3f0408e0f96887b739f4717f458ce41bdf1c6071
ManduTheCat <krocd@naver.com>
2022-10-10 21:07:22 +0900

    swea 등산로 1949 풀이 원숭이 풀이로는 안됨 안되는 풀이 왜 안되지?

--- javaSolve/src/algorithmStudy/week9/hiking1949swea/Solution.java
+++ javaSolve/src/algorithmStudy/week9/hiking1949swea/Solution.java
@@ -59,12 +59,13 @@ public class Solution {
                     map[row][col] = Integer.parseInt(st.nextToken());
                 }
             }
-            //printMap();
+            //최대 깊이찾기
             ArrayList<Point> startPoints = findTop();
             maxDepth = 0;
             for (Point startPoint : startPoints) {
-                //System.out.println(startPoint.value);
                 check = new boolean[N][N][2];
+                check[startPoint.r][startPoint.c][0] =true;
+                check[startPoint.r][startPoint.c][1] =true;
                 dfs(startPoint);
 
             }
@@ -75,30 +76,30 @@ public class Solution {
     }
 
     private static void dfs(Point currPoint) {
-        //System.out.println(currPoint);
         maxDepth = Math.max(maxDepth, currPoint.depth);
         for (int d = 0; d < 4; d++) {
             int nr = currPoint.r + del[d][0];
             int nc = currPoint.c + del[d][1];
             if (isIn(nr, nc)) {
                 int nextValue = map[nr][nc];
+                // 여기는 무조건 파는 경우 이기 떄문에 k 1부터 시작함
                 for (int k = 1; k <= K; k++) {
+                    // 팟을때 지금 것보다 작다면 && 팔수 있는 횟수가 남아 있다면 && 방문핸던적이 없다면
                     if (nextValue - k < currPoint.value && currPoint.drillCount > 0
-                            && !check[currPoint.r][currPoint.c][currPoint.drillCount-1]) {
-                        // 안에 들어오고 다음것을  k 만큼 파버렸을때 나보다 작다면
-                        //System.out.println(nextValue+" - "+ k +" " +(nextValue - k) +" < "+ currPoint.value);
-                        check[currPoint.r][currPoint.c][currPoint.drillCount-1] = true;
+                            && !check[nr][nc][currPoint.drillCount-1]) {
+                        // 원숭이처럼 드릴카운트 하나 까고 넘긴다
+                        check[nr][nc][currPoint.drillCount-1] = true;
                         dfs(new Point(nr, nc, Math.max(map[nr][nc] - k, 0),
                                 currPoint.depth + 1, currPoint.drillCount - 1));
-                        check[currPoint.r][currPoint.c][currPoint.drillCount - 1] = false;
+                        check[nr][nc][currPoint.drillCount - 1] = false;
 
                     }
                 }
-                if (nextValue < currPoint.value && !check[currPoint.r][currPoint.c][currPoint.drillCount]) {
-                    // 안에들어오고 다음 것이 작다면
-                    check[currPoint.r][currPoint.c][currPoint.drillCount] = true;
+                // 드릴을 사용하지 않는경우
+                if (nextValue < currPoint.value && !check[nr][nc][currPoint.drillCount]) {
+                    check[nr][nc][currPoint.drillCount] = true;
                     dfs(new Point(nr, nc, map[nr][nc], currPoint.depth + 1, currPoint.drillCount));
-                    check[currPoint.r][currPoint.c][currPoint.drillCount] = false;
+                    check[nr][nc][currPoint.drillCount] = false;
                 }
             }
         }
