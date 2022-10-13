91bc2516038169c9918910d3b22623fecbbcd4b8
김명진 <krocd@naver.com>
2022-10-14 02:11:38 +0900

    swea 원점으로 가기 8458

--- javaSolve/src/algorithmStudy/week9/findCrystalBallBJ2617/Main.java
+++ javaSolve/src/algorithmStudy/week9/findCrystalBallBJ2617/Main.java
@@ -19,10 +19,14 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         forwardAdjList = new ArrayList[N + 1];
         backwardAdjList = new ArrayList[N + 1];
+        // arrayList 선언
         for (int vertex = 1; vertex <= N; vertex++) {
             forwardAdjList[vertex] = new ArrayList<>();
             backwardAdjList[vertex] = new ArrayList<>();
         }
+        // 큰관계 작은 관계 탐색을위한 인접 리스트 생성
+        // 사실 간선수가 많은 완전그래프면 인접 행렬의 장점이 있지만
+        // 간선수가 완전이 아니니 인접리스트를 사용함
         for (int edge = 0; edge < M; edge++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
@@ -30,7 +34,6 @@ public class Main {
             forwardAdjList[from].add(to);
             backwardAdjList[to].add(from);
         }
-
         int[] forwardMap = new int[N+1];
         int[] backwardMap = new int[N+1];
 
@@ -40,6 +43,8 @@ public class Main {
             forwardMap[startNode] = forwardRes;
             backwardMap[startNode] = backwardRes;
         }
+        System.out.println(Arrays.toString(forwardMap));
+        System.out.println(Arrays.toString(backwardMap));
         int count = 0;
         for(int forwardCount : forwardMap){
             if(forwardCount > N/2)count++;
@@ -47,6 +52,7 @@ public class Main {
         for(int backward : backwardMap){
             if(backward > N/2)count++;
         }
+
         System.out.println(count);
     }
 
@@ -65,6 +71,7 @@ public class Main {
                 }
             }
         }
+
         int count = 0;
         for(boolean el : check){
             if(el)count++;
