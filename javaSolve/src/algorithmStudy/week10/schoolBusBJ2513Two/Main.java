73de0a3dd91b84aba7515918ac16d403e5e88bb6
김명진 <krocd@naver.com>
2022-10-21 07:20:47 +0900

    통학버스 객체로 풀기

---
+++ javaSolve/src/algorithmStudy/week10/schoolBusBJ2513Two/Main.java
@@ -0,0 +1,85 @@
+package algorithmStudy.week10.schoolBusBJ2513Two;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+class Bus {
+    int maxStudent;
+    int currStudent;
+
+    public Bus(int maxStudent, int currStudent) {
+        this.maxStudent = maxStudent;
+        this.currStudent = currStudent;
+    }
+
+    @Override
+    public String toString() {
+        return "Bus{" +
+                "maxStudent=" + maxStudent +
+                ", currStudent=" + currStudent +
+                '}';
+    }
+}
+class AptPoint implements Comparable<AptPoint>{
+    int studentNum;
+    int dist;
+    int point;
+
+    public AptPoint(int studentNum, int dist, int point) {
+        this.studentNum = studentNum;
+        this.dist = dist;
+        this.point = point;
+    }
+
+    @Override
+    public String toString() {
+        return "AptPoint{" +
+                "studentNum=" + studentNum +
+                ", dist=" + dist +
+                ", point=" + point +
+                '}';
+    }
+
+    @Override
+    public int compareTo(AptPoint o) {
+        return this.dist - o.dist;
+    }
+}
+
+public class Main {
+    static int N, K, S;
+    static int sum = 0;
+    static PriorityQueue<AptPoint> left;
+    static PriorityQueue<AptPoint> right;
+    static Bus bus;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("resources/study/week10/BJ2513/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken()); // 지점수
+        K = Integer.parseInt(st.nextToken()); // 인원수
+        S = Integer.parseInt(st.nextToken()); // 학교위치
+        bus = new Bus(K, 0);
+        for (int row = 0; row < N; row++) {
+            st = new StringTokenizer(br.readLine());
+            int point = Integer.parseInt(st.nextToken());
+            int student = Integer.parseInt(st.nextToken());
+
+
+        }
+
+
+    }
+
+    public static void findLeft() {
+
+    }
+
+
+}
