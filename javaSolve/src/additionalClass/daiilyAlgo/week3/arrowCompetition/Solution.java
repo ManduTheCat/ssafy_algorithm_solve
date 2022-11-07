c2208468796191f0d2a969a3e22ece0681adb05a
ManduTheCat <krocd@naver.com>
2022-11-07 10:20:59 +0900

    양궁대회 주석 추가

--- javaSolve/src/additionalClass/daiilyAlgo/week3/arrowCompetition/Solution.java
+++ javaSolve/src/additionalClass/daiilyAlgo/week3/arrowCompetition/Solution.java
@@ -15,11 +15,17 @@ public class Solution {
         INFO = info.clone();
         lionWinCase = new ArrayList<int[]>();
         // 타겟점수 를선택하는 컴비네이션 구하기
-        // i번째 화살이 면번 타겟을 맞췄는지
+        // i번째 화살이 몇번 타겟을 맞췄는지
         combinationRes = new int[N]; // 0~11
+        // 화살을 쏘는 경우의 수를 구하는 함수
+        // 경우의 수가 나올때마다 점수를 계산
+        // info 와 같은 array 를 만든다
         combination(0, 0);
+        //priorityQueue 를 활용해 가장 차이가 많이나는 이기는 경우의 수를 찾는다.
+        findMaxDiff(); // 
+        // 결과를 리턴하기 전에
+        // 만약 이기는 경우가 여러개일때 작은 점수를 많이쏜 2가지 경우를 찾는 함수
 
-        findMaxDiff();
         return getShotMinTarget();
 
     }
@@ -31,24 +37,21 @@ public class Solution {
         if(lionWinCase.size() == 1){
             return  lionWinCase.get(0);
         }
-
-        // 여러개라면 인덱스를 가중치로 했을때 점수가 높은 친구를 리턴한다.
+        // 같은 점수일땐 작은 점수를 많이 쏜 경우를 반환해야함으로
+        // 인덱스를 가중치로  삼아 했을때 점수가 높은 친구를 리턴한다.
         int [] maxRes = new int[11];
         int maxWightScore = 0;
         for(int [] res:lionWinCase){
             int currWightScore = 0;
             for (int i = 0; i < res.length; i++) {
-                currWightScore += (i + 1)* res[i];
+                currWightScore += (i + 1)* res[i]; // 0을 곱하면 안되서 +1
             }
             if(currWightScore > maxWightScore){
                 maxRes = res.clone();
                 maxWightScore = currWightScore;
             }
-
-
         }
         return maxRes;
-
     }
 
     private void findMaxDiff() {
@@ -68,8 +71,7 @@ public class Solution {
 
     private void combination(int depth , int start) {
         if(depth ==N){
-            //System.out.println(Arrays.toString(combinationRes));
-            calculateScore();
+            calculateScore();// 경우의 수가 나올때마다 점수를 계산
             return;
         }
         // 중복을 허용하는 컴비네이션
@@ -80,16 +82,14 @@ public class Solution {
     }
 
     private void calculateScore() {
+        // 화살을 쏜 결과를 가지고 점수를 계산하는 함수
         int[] res = new int[11];
         int lionScore = 0;
         int apeachScore = 0;
 
-        // 10 - i 인덱스에 값이 들어간다.
         for(int targetScore : combinationRes){
             res[10 - targetScore] +=1;
         }
-        //System.out.println("res : "+Arrays.toString(res));
-        //System.out.println("info : "+ Arrays.toString(INFO));
         //큰쪽이 점수가져간다
         // 같으면 // 0일때 점수 없다 // 0 이 아니면 어피치가 점수가져간다.
         for (int i = 0; i < 11; i++) {
@@ -104,11 +104,12 @@ public class Solution {
                 apeachScore+=10-i;
             }
         }
-        //System.out.println( "어피치점수 : "+ apeachScore + " 라이언 점수 : " + lionScore + (lionScore > apeachScore? " 라이언 우승":" 어피치 우승"));
         if(lionScore > apeachScore){
             findMaxQue.offer(new ShotListANDDiff(lionScore - apeachScore, res));
         }
     }
+
+    // 차와 화살을쏜 결과를 보관하기 위한 class  pq 사용하기 위해 comparable implements
     static class ShotListANDDiff implements Comparable<ShotListANDDiff>{
         int diff;
         int [] lionRes;
