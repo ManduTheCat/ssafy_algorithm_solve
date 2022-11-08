e37e561491283aded4a7fa4a1df79307ed479ba9
ManduTheCat <krocd@naver.com>
2022-11-08 09:08:11 +0900

    순위 매기기 rank init

---
+++ javaSolve/src/additionalClass/daiilyAlgo/week4/rankSearch/Main.java
@@ -0,0 +1,7 @@
+package additionalClass.daiilyAlgo.week4.rankSearch;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ javaSolve/src/additionalClass/daiilyAlgo/week4/rankSearch/Solution.java
@@ -0,0 +1,8 @@
+package additionalClass.daiilyAlgo.week4.rankSearch;
+
+public class Solution {
+    public int[] solution(String[] info, String[] query) {
+        int[] answer = {};
+        return answer;
+    }
+}
