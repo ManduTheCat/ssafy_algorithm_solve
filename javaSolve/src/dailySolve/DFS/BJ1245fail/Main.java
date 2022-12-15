74cc4da2837e1e700396ce70dd82fded8dd6b837
ManduTheCat <krocd@naver.com>
2022-12-16 00:33:27 +0900

    1245 농장 컷

---
+++ javaSolve/src/dailySolve/DFS/BJ1245fail/Main.java
@@ -0,0 +1,109 @@
+package dailySolve.DFS.BJ1245fail;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Cordi {
+    int r;
+    int c;
+
+    public Cordi(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cordi{" +
+                "r=" + r +
+                ", c=" + c +
+                '}';
+    }
+}
+
+public class Main {
+    static int[][] dArr = {{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
+    static int N;
+    static int M;
+    static int[][] map;
+    static boolean[][] check;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("resources/daily/bfs/BJ1245/input4.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        check = new boolean[N][M];
+        for (int row = 0; row < N; row++) {
+            st = new StringTokenizer(br.readLine());
+            for (int col = 0; col < M; col++) {
+                map[row][col] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int count = 0;
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < M; col++) {
+                if (!check[row][col] && map[row][col] != 0) {
+                    // 이전에 방문 했던 봉우리면 다시 가지 않는다. 0 은 봉우리가 아니다
+                    boolean result = bfs(new Cordi(row, col));
+                    if (result) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.print(count);
+
+    }
+
+    private static boolean bfs(Cordi startCordi) {
+        Queue<Cordi> q = new ArrayDeque<>();
+        q.offer(startCordi);
+        boolean[][] bfsCheck = new boolean[N][M];
+        bfsCheck[startCordi.r][startCordi.c] = true;
+        while (!q.isEmpty()) {
+            Cordi curr = q.poll();
+            for (int d = 0; d < 8; d++) {
+                int nextR = curr.r + dArr[d][0];
+                int nextC = curr.c + dArr[d][1];
+                // 자기랑 같은 친구들만 방문한내
+                if (isIn(nextR, nextC) && !bfsCheck[nextR][nextC] && map[nextR][nextC] == map[curr.r][curr.c]) {
+                    System.out.println("in");
+                    if (isTop(nextR, nextC)) {
+                        // 8방 탐색 해서 자기보다 주변이 작거나 같을때만 넣는다. 나보다 높을땐 false
+                        bfsCheck[nextR][nextC] = true;
+                        q.offer(new Cordi(nextR, nextC));
+                        check[nextR][nextC] = true;
+                    } else return false;
+
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isTop(int curR, int curC) {
+        // 8 방탐색 진행해서 자기보다 큰값 존재하면 false
+        for (int d = 0; d < 8; d++) {
+            int checkR = curR + dArr[d][0];
+            int checkC = curC + dArr[d][1];
+            if (isIn(checkR, checkC) && map[checkR][checkC] > map[curR][curC]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isIn(int nextR, int nextC) {
+        return nextR < N && nextR >= 0 && nextC < M && nextC >= 0;
+    }
+
+}
---
+++ javaSolve/src/dailySolve/DFS/BJ1245sec/Main.java
@@ -0,0 +1,126 @@
+package dailySolve.DFS.BJ1245sec;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Cordi {
+    int r;
+    int c;
+
+    public Cordi(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Cordi{" +
+                "r=" + r +
+                ", c=" + c +
+                '}';
+    }
+}
+
+public class Main {
+    static int[][] dArr = {{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
+    static int N;
+    static int M;
+    static int[][] map;
+    static boolean[][] check;
+
+    public static void main(String[] args) throws IOException {
+        System.setIn(new FileInputStream("resources/daily/bfs/BJ1245/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        check = new boolean[N][M];
+        for (int row = 0; row < N; row++) {
+            st = new StringTokenizer(br.readLine());
+            for (int col = 0; col < M; col++) {
+                map[row][col] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int count = 0;
+        for (int row = 0; row < N; row++) {
+            for (int col = 0; col < M; col++) {
+                if (!check[row][col] && map[row][col] != 0) {
+                    // 이전에 방문 했던 봉우리면 다시 가지 않는다. 0 은 봉우리가 아니다
+                    //System.out.println("search "+ row +", "+ col);
+                    boolean result = bfs(new Cordi(row, col));
+                    //System.out.println(result);
+                    if (result) {
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.print(count);
+
+    }
+
+    private static boolean bfs(Cordi startCordi) {
+        Queue<Cordi> q = new ArrayDeque<>();
+        q.offer(startCordi);
+        if(!isTop(startCordi.r, startCordi.c)){
+            return false;
+        }
+        boolean[][] bfsCheck = new boolean[N][M];
+        bfsCheck[startCordi.r][startCordi.c] = true;
+        while (!q.isEmpty()) {
+            Cordi curr = q.poll();
+            for (int d = 0; d < 8; d++) {
+                int nextR = curr.r + dArr[d][0];
+                int nextC = curr.c + dArr[d][1];
+                // 자기랑 같은 친구들만 방문한내
+                if (isIn(nextR, nextC) && !bfsCheck[nextR][nextC] && map[curr.r][curr.c] == map[nextR][nextC]) {
+                    if (isTop(nextR, nextC)) {
+                        // 8방 탐색 해서 자기보다 주변이 작거나 같을때만 넣는다. 나보다 높을땐 false
+                        bfsCheck[nextR][nextC] = true;
+                        q.offer(new Cordi(nextR, nextC));
+                        check[nextR][nextC] = true;
+                    } else return false;
+
+                }
+            }
+        }
+        //printArr(bfsCheck);
+        // curr 의 주변은 체크를 안합니다?
+        return true;
+    }
+
+    private static boolean isTop(int curR, int curC) {
+        // 8 방탐색 진행해서 자기보다 큰값 존재하면 false
+        for (int d = 0; d < 8; d++) {
+            int checkR = curR + dArr[d][0];
+            int checkC = curC + dArr[d][1];
+//            System.out.println("im check this " +map[checkR][checkC]);
+            if (isIn(checkR, checkC) && map[checkR][checkC] > map[curR][curC]) {
+                //System.out.println("touch around hight");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isIn(int nextR, int nextC) {
+        return nextR < N && nextR >= 0 && nextC < M && nextC >= 0;
+    }
+
+    private static void printArr(boolean[][] arr) {
+        for (boolean[] row : arr) {
+            for (boolean el : row) {
+                System.out.print(el ? 1 + " " : 0 + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
