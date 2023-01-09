af4c0582c18d442e6f320364c4ddb1ef639605e5
ManduTheCat <krocd@naver.com>
2023-01-10 01:48:08 +0900

    다익스트라 수정

--- javaSolve/src/dailySolve/dijkstra/minCost1916/Main.java
+++ javaSolve/src/dailySolve/dijkstra/minCost1916/Main.java
@@ -53,10 +53,10 @@ public class Main {
             adjList[n] = new ArrayList<Node>();
             dp[n] = Integer.MAX_VALUE;
         }
-        dp[0] = 0;
         for (int m = 0; m < M; m++) {
             // 마이너스 1은 별로
             // 인덱스라는 명칭이 올바른거 같다
+            // 명치 디스트로
             StringTokenizer st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken()) - 1;
             int end = Integer.parseInt(st.nextToken()) - 1;
@@ -67,12 +67,8 @@ public class Main {
         int start = Integer.parseInt(st.nextToken()) - 1;
         int target = Integer.parseInt(st.nextToken()) - 1;
         dijkstra(start);
+        dp[start] = 0;
 
-//        for (int n = 0; n < N; n++) {
-//            ArrayList<Node> list = adjList[n];
-//            System.out.println(n);
-//            System.out.println(list);
-//        }
         System.out.println(dp[target]);
     }
 
@@ -80,7 +76,7 @@ public class Main {
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.add(new Node( start, 0));
 
-        while (!pq.isEmpty()) {
+        while (!pq.isEmpty()) {//E
             Node curr = pq.poll();
 //            System.out.println(curr);
 
@@ -89,7 +85,7 @@ public class Main {
             for (Node nextNode : adjList[curr.index]) {
 
                 // 다음 노드는 방문하지 않고 최소야한다
-                if (dp[nextNode.index] > nextNode.weight + dp[curr.index]) {
+                if (dp[nextNode.index] > nextNode.weight + dp[curr.index]) {//E //v
                     // 다음 이동 할 정점까지의 비용을 갱신한다.
                     dp[nextNode.index] = dp[curr.index] + nextNode.weight;
                     pq.add(new Node( nextNode.index, dp[nextNode.index]));
