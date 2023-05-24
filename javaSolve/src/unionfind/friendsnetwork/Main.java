e2a7fc29523703674b3d4bcb0fb58fab453ca3fb
ManduTheCat <krocd@naver.com>
2023-05-24 17:51:03 +0900

    4195 유니온 작성중

---
+++ javaSolve/src/unionfind/friendsnetwork/Main.java
@@ -0,0 +1,42 @@
+package unionfind.friendsnetwork;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+	static Map<String, String> parents;
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./resources/daily/unionfind/4195/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Integer testCount = Integer.parseInt(br.readLine());
+		for (int t = 0; t < testCount; t++) {
+			int f = Integer.parseInt(br.readLine());
+			for (int relastion = 0; relastion < f; relastion++) {
+				parents = new HashMap();
+
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				String friendsA = st.nextToken();
+				String friendsB = st.nextToken();
+				System.out.println(friendsA + " "+ friendsB);
+			}
+		}
+
+	}
+	public static String find(String name){
+		if(parents.get(name).equals(name)){
+			return name;
+		}
+		return parents.put(parents.get(name), find(name));
+	}
+	public static boolean union(String a, String b){
+		if(find(a).equals(find(b))){
+			return false;
+		}
+		return true;
+	}
+}
