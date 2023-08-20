00f83e9fa43577d656b4c8305126bdb4dbba4a38
ManduTheCat <krocd@naver.com>
2023-08-21 00:09:37 +0900

    BBJ28703 double it 보고품..

---
+++ javaSolve/src/greedy/BJ28703/Main.java
@@ -0,0 +1,40 @@
+package greedy.BJ28703;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int [] inputs;
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./resources/daily/greedy/BJ28703/input.txt"));
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		inputs = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int initMax = Integer.MIN_VALUE;
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for(int n = 0; n < N; n++){
+			int value = Integer.parseInt(st.nextToken());
+			initMax = Math.max(initMax,value);
+			pq.add(value);
+		}
+		// System.out.println(pq);
+		int max = initMax;
+		int res = max - pq.peek();
+		while (pq.peek() < initMax ){
+			int minCandidate = pq.poll();
+			res = Math.min(max - minCandidate, res);
+			int next = minCandidate * 2;
+			max = Math.max(next, max);
+			pq.offer(next);
+		}
+		res = Math.min(res, max - pq.peek());
+		System.out.println(res);
+	}
+}
