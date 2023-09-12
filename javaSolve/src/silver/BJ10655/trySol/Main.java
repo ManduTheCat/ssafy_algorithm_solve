4e6fe35c3b6db8e9e0eac62abf5754867297a484
ManduTheCat <krocd@naver.com>
2023-09-13 02:15:30 +0900

    마라톤 1 10655 구현, 수학 풀이

--- javaSolve/src/silver/BJ10655/trySol/Main.java
+++ javaSolve/src/silver/BJ10655/trySol/Main.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+
 class Cordi {
 	public int x , y;
 	Cordi(int x , int y){
@@ -37,8 +38,37 @@ public class Main {
 			int y = Integer.parseInt(st.nextToken());
 			inputs.add(new Cordi(x, y));
 		}
+		int sum = 0;
+		for(int n = 0; n < N - 1; n++){
+			Cordi curr = inputs.get(n);
+			Cordi next = inputs.get(n+1);
+			int val = Math.abs(curr.x - next.x) + Math.abs(curr.y - next.y);
+			sum += val;
+			// System.out.println(val);
+		}
+		// System.out.println(sum);
+
+		for(int delIdx  = 1; delIdx < N - 1; delIdx++){
+			int val = deleteLength(delIdx, sum);
+			min  = Math.min(min, val);
+		}
+
 
 		System.out.println(min);
+	}
+	public static int deleteLength(int delIdx, int totalLen){
+		// System.out.println("total :" + totalLen + " idx :" + delIdx );
+		Cordi curr = inputs.get(delIdx);
+		Cordi next = inputs.get(delIdx + 1);
+		Cordi before = inputs.get(delIdx - 1);
+		int currBefore = Math.abs(curr.x - before.x) + Math.abs(curr.y - before.y);
+		int currNext = Math.abs(curr.x - next.x) + Math.abs(curr.y - next.y);
+		int beforeNext = Math.abs(before.x - next.x) + Math.abs(before.y - next.y);
+		// System.out.println("before to curr:" + currBefore);
+		// System.out.println("curr to next:" + currNext);
+		// System.out.println("before to next: " + beforeNext);
+		return (totalLen - currBefore - currNext) + beforeNext;
+
 	}
 
 }
