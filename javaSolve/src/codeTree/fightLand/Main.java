b4c725c4c671b9a86c23e82ec1b64b60dea2538d
ManduTheCat <krocd@naver.com>
2023-09-20 00:37:01 +0900

    싸움땅 중간 round2 가 안된다

--- javaSolve/src/codeTree/fightLand/Main.java
+++ javaSolve/src/codeTree/fightLand/Main.java
@@ -11,9 +11,13 @@ class Player {
 	int col;
 
 	int d;
-	int s;
+	private int s;
 	int w;
 	int idx;
+
+	public int getS() {
+		return s;
+	}
 
 	public Player(int row, int col, int d, int s, int w, int idx) {
 		this.row = row;
@@ -70,11 +74,12 @@ public class Main {
 			int s = Integer.parseInt(st.nextToken());
 			players[m] = new Player(x - 1, y - 1, d, s, 0, m);
 		}
-		while (K-- > 0){
+		// K+=1;
+		while (K-- > 0) {
 			// round1
+			System.out.println("round " + K);
 			for (Player p : players) {
 				System.out.println(p + " 가 이동합니다.");
-				printPLayer();
 				// 앞으로 가기
 				if (!forward(p)) {// 가능하면 이동한다.
 					// 벽에 부딪친다면 반대 방향으로 돌고 앞으로 한칸 == 두번 dir 돌리고 앞으로 한칸.
@@ -83,37 +88,40 @@ public class Main {
 					int nextCol = p.col + dir[nextDirIdx][1];
 					p.row = nextRow;
 					p.col = nextCol;
+					p.d = nextDirIdx;
 				}
 				for (Player enemy : players) {// 자기자신을 제외해야한다.........
 					if (p.row == enemy.row && p.col == enemy.col && p.idx != enemy.idx) {
 						//싸운다. 총은 줍지 않는다.
 						System.out.println(p + " 차례에 " + enemy + " 와 싸웁니다.");
 						// printMap();
-						System.out.println("fight");
 						fight(p, enemy);
-
 						// printMap();
 					} else {// 부딪친 사람이 없다면
 						// 총을 줍는다.
 						getGun(p);
 					}
 				}
+				printPLayer();
 
 			}
+			for (Player p : players) {
+				System.out.println(p);
+			}
 		}
+
+		System.out.println("=====res=======");
+		printMap();
 		printPLayer();
-		for (Player p : players) {
-			System.out.println(p);
-		}
 		System.out.println(Arrays.toString(point));
 	}
 
 	public static void fight(Player p, Player enemy) {
-		int pPower = p.s + p.w;
-		int ePower = enemy.s + enemy.w;
+		int pPower = p.getS() + p.w;
+		int ePower = enemy.getS() + enemy.w;
 		if (pPower > ePower) {
 			// 왼쪽이 이긴경우
-			point[p.idx] = Math.abs(pPower - ePower);
+			point[p.idx] += Math.abs(pPower - ePower);
 			int dropWp = enemy.w; // 진사람 총을 땅에 떨군다
 			enemy.w = 0; // 진사람은 무기가 없다.
 			if (p.w < dropWp) { // 가지고 있는 총보다 떨군총이 더 크다면
@@ -121,9 +129,10 @@ public class Main {
 				p.w = dropWp; // 떨군걸 가져간다.
 			}// 진사람 이동해야한다.
 			loseMove(enemy);
+			getGun(enemy);
 		} else if (pPower < ePower) {
 			// 오른쪽이 이긴경우
-			point[enemy.idx] = Math.abs(ePower - pPower);
+			point[enemy.idx] += Math.abs(ePower - pPower);
 			int dropWp = p.w;
 			p.w = 0;
 			if (enemy.w < dropWp) {
@@ -131,10 +140,11 @@ public class Main {
 				enemy.w = dropWp;
 			}// 진사람 이동해야한다
 			loseMove(p);
+			getGun(p);
 		} else {
 			// 비긴경우 초기 능력치를 비교한다.
 			// 각 플레이어의 초기 능력치와 가지고 있는 총의 공격력의 합의 차이만큼을 포인트로 획득하게 됩니다. = 0
-			if (p.s > enemy.s) {
+			if (p.getS() > enemy.getS()) {
 				// 포인트 증가는 없다
 				int dropW = enemy.w;
 				enemy.w = 0;
@@ -143,6 +153,7 @@ public class Main {
 					p.w = dropW;
 				}
 				loseMove(enemy);
+				getGun(enemy);
 
 			} else {
 				//초기 능력치는 모두 다릅니다 -> enemy 승
@@ -155,14 +166,18 @@ public class Main {
 					enemy.w = dropW;
 				}
 				loseMove(p);
+				getGun(p);
 
 			}
 		}
+
+		System.out.println(Arrays.toString(point));
 	}
 
 	public static void loseMove(Player lose) {
+		System.out.println("진사람 " + lose + " 이동합니다.");
 		// 90 도씩 돌려보며 가능하면 이동한다. 그리고 총줍느다.
-		if(!forward(lose)){
+		if (!forward(lose)) {
 			for (int add = 1; add < 4; add++) { // 1~3 나다음 3방향 까지만 보면된다
 				int nextDir = (lose.d + add) % 4;
 				int nextRow = lose.row + dir[nextDir][0];
@@ -170,6 +185,7 @@ public class Main {
 				if (isIn(nextRow, nextCol)) {
 					lose.row = nextRow;
 					lose.col = nextCol;
+					lose.d = nextDir;
 					break;
 				}
 			}
@@ -195,7 +211,7 @@ public class Main {
 				boolean flag = false;
 				for (Player p : players) {
 					if (p.row == row && p.col == col) {
-						System.out.print((p.idx + 1) + " ");
+						System.out.print((p.idx) + " ");
 						flag = true;
 						break;
 					}
