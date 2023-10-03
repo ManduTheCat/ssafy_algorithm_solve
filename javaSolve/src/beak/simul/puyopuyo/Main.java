ced669e21481f9a565cd9c80a1087bb774c2d78d
ManduTheCat <krocd@naver.com>
2023-10-04 06:33:12 +0900

    뿌요뿌요 리트

--- javaSolve/src/beak/simul/puyopuyo/Main.java
+++ javaSolve/src/beak/simul/puyopuyo/Main.java
@@ -6,109 +6,119 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Queue;
+import java.util.StringTokenizer;
+
+class Cordi{
+	int row;
+	int col;
+
+	public Cordi(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+
+	@Override
+	public String toString() {
+		return "Cordi{" +
+			"row=" + row +
+			", col=" + col +
+			'}';
+	}
+}
 public class Main {
-	// 터트리고 내려야한다.
-	// 터트리는걸 어떻게 할건가
-	// 1초
-	// 터진 친구들의 위치을 기록했다가
-	public static String [][] board;
-	public static boolean[][] check;
-	public static final int ROW = 12;
-	public static final int COL = 6;
+	static int R = 12;
+	static int C = 6;
+	static boolean [][] check;
+	static String [][] map;
+	// 1 같은거 4개 처리
+	// 2 중력 작용
+	// 3 터질게 있나 검사
 	public static void main(String[] args) throws IOException {
-		FileInputStream fileInputStream = new FileInputStream("resources/additionalClass/beak/sim/pyuo/input.txt");
-		System.setIn(fileInputStream);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		board = new String[ROW][COL];
-		check = new boolean[ROW][COL];
-		for (int row = 0; row < ROW; row++) {
-			String readRow = br.readLine();
-			for (int col = 0; col < COL; col++) {
-				String data = String.valueOf(readRow.charAt(col));
-				board[row][col] = data;
-				if(data.equals(".")) check[row][col] = true;
+
+		System.setIn(new FileInputStream("resources/daily/sim/puyoPyuo/input.txt"));
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+		map = new String[R][C];
+		check = new boolean[R][C];
+		for(int row = 0; row < R; row++){
+			String[] line = br.readLine().split("");
+			for(int col = 0; col < C; col++){
+				map[row][col] = line[col];
+				if(line[col].equals(".")){
+					check[row][col] = true;
+				}
 			}
 		}
-		// 돌면서 부슨다. 리턴을 불리언으로 ?
-		for (int row = 0; row < ROW; row++) {
-			for (int col = 0; col < COL; col++) {
+		delete(); // 와일 델리트
+		check = makeNewCheck();
+
+
+	}
+	public static boolean[][] makeNewCheck(){
+		boolean[][] res = new boolean[R][C];
+		for(int row = 0; row < R; row++){
+			for(int col = 0; col < C; col++){
+				String val = map[row][col];
+				if(val.equals(".")){
+					res[row][col] = true;
+				}
+
+			}
+		}
+		return res;
+	}
+	public static boolean delete(){
+		boolean res = false;
+		for(int row = 0; row < R; row++){
+			for(int col = 0; col < C; col++){
 				if(!check[row][col]){
-					String startColor = board[row][col];
-					breakThings(new Cordi(row, col), startColor);
+					List<Cordi> deleteCandi = findGroup(new Cordi(row, col));
+					System.out.println(deleteCandi);
+					if(deleteCandi.size() >= 4){
+						res = true;
+						for(Cordi target: deleteCandi){
+							map[target.row][target.col] = ".";
+						}
+					}
 				}
 			}
 		}
-		// 땡긴다
-		// 검사하기 false 면 계속 부슨다.
-
-	}
-	public static  void printMap(String[][] map){
-		for (String[] row : map) {
-			System.out.println(Arrays.toString(row));
-		}
-	}
-	public static  void printMap(boolean[][] map){
-		for (boolean[] row : map) {
-			System.out.println(Arrays.toString(row));
-		}
+		return res;
 	}
 
-
-	static class Cordi {
-		int row;
-		int col;
-
-		public Cordi(int row, int col) {
-			this.row = row;
-			this.col = col;
-		}
-
-		@Override
-		public String toString() {
-			return "Cordi{" +
-				"row=" + row +
-				", col=" + col +
-				'}';
-		}
-	}
-	public static void breakThings(Cordi startPoint, String startColor){
+	public static List<Cordi> findGroup(Cordi start){
+		int count = 1;
+		List<Cordi> res = new ArrayList<>();
+		res.add(start);
 		Queue<Cordi> q = new ArrayDeque<>();
-		// 4방 탐색
-		int [][] dir  = {{0, -1},{-1, 0},{0,1},{1,0}};
-		List<Cordi> boomCandidate = new ArrayList<>();
-		// 위에서 부터 아래로 간단 // 제거된건 V 로 처리
-		q.add(new Cordi(startPoint.row,startPoint.col));
-		int sameCount = 0;
+		q.add(start);
+		String startSt = map[start.row][start.col];
+		check[start.row][start.col] = true;
+		int[][] dir = {{0,-1},{-1,0},{0,1},{1,0}};
 		while (!q.isEmpty()){
-			// 같은 친구면 제거
-
-			Cordi cur = q.poll();
-			for (int d = 0; d < 4; d++) {
-				int nextRow = cur.row + dir[d][0];
-				int nextCol = cur.col + dir[d][1];
-				if(isIn(nextRow, nextCol) && !check[nextRow][nextCol]
-					&& board[nextRow][nextCol].equals(startColor)){
+			Cordi curr = q.poll();
+			for(int [] d : dir){
+				int nextRow = curr.row + d[0];
+				int nextCol = curr.col +d[1];
+				if(isIn(nextRow, nextCol) && !check[nextRow][nextCol] && map[nextRow][nextCol].equals(startSt)){
+					Cordi next = new Cordi(nextRow, nextCol);
+					q.add(next);
 					check[nextRow][nextCol] = true;
-					Cordi nextCordi = new Cordi(nextRow, nextCol);
-					q.add(nextCordi);
-					boomCandidate.add(nextCordi);
-					sameCount ++;
+					count++;
+					res.add(next);
 				}
 			}
+
 		}
-		// 카운트를 기반으로 4개 이상이면 처리한다.
-		if(sameCount >= 4){
-			for(Cordi boomTarget : boomCandidate){
-				board[boomTarget.row][boomTarget.col] = "V";
-			}
-			printMap(board);
+		if(count >=4) {
+			return res;
 		}
+		return new ArrayList<>();
+
+	}
+	public static boolean isIn(int row, int col){
+		return row >= 0 && row < R && col >= 0 && col < C;
 	}
 
-	private static boolean isIn(int nextRow, int nextCol) {
-		return nextRow >= 0 && nextRow < ROW && nextCol >= 0 && nextCol < COL;
-	}
 }
