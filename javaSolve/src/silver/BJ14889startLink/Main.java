2f3ea62d68ee32c4e721a6adacebec1a51827889
김명진 <krocd@naver.com>
2023-12-29 00:27:08 +0900

    하지마하지마

--- javaSolve/src/silver/BJ14889startLink/Main.java
+++ javaSolve/src/silver/BJ14889startLink/Main.java
@@ -33,14 +33,16 @@ public class Main {
 	public static void divideTeam(int start, int depth){// 조합 구하기 N 개중에서 N/2 선택하기
 		if(depth == N/2){
 
-			System.out.println(Arrays.toString(teamPool1));
+			teamPool2 = makePool2(teamPool1);
+			System.out.println(Arrays.toString(teamPool1) + " vs " + Arrays.toString(teamPool2));
+			System.out.println(cal());
 			return;
 		}
 		for(int i = start;  i < N; i++){
 			teamPool1[depth] = i;
 			divideTeam(i+1, depth+1);
 		}
-
+		//2 3 1 4
 	}
 	public static int[] makePool2(int[] poo1){
 		Set<Integer> team2 = new HashSet<>();
@@ -58,6 +60,22 @@ public class Main {
 			idx++;
 		}
 		return res;
+	}
+	public static int  cal  (){
+		int team1sum = 0;
+		for(int i = 0; i < teamPool1.length; i++){
+			for(int j = i + 1; j <teamPool1.length; j++){
+				team1sum +=synergy[teamPool1[i]][teamPool1[j]];
+			}
+		}
+		int team2sum = 0;
+		for(int i = 0; i < teamPool2.length; i++){
+			for(int j = i + 1; j <teamPool2.length; j++){
+				team2sum +=synergy[teamPool2[i]][teamPool2[j]];
+			}
+		}
 
+		System.out.println(team1sum  + " vs " + team2sum);
+		return Math.abs(team1sum - team2sum);
 	}
 }
