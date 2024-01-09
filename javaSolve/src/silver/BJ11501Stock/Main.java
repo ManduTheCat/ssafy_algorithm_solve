9d525b7622b4b288a10d6a52e3608cd1685d6b0c
김명진 <krocd@naver.com>
2024-01-10 02:00:25 +0900

    주식

---
+++ javaSolve/src/silver/BJ11501Stock/Main.java
@@ -0,0 +1,19 @@
+package silver.BJ11501Stock;
+
+
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    // 증분값을 구한다.
+    // 증분값중 증가할때 다음도 증가하는지 확인한다.
+    // 증분값 감소하면 1차 컷
+    // 다시 증분값 증가하는 패턴을 찾는다
+    // 증분값 패턴중 가장 큰 값 에서 왼쪽 값들을 뺀다 수익에 넣는다.
+    static int T;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+}
