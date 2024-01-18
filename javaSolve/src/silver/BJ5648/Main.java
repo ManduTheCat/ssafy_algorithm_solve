468cd2482188c0dc76d8c8176d83e6abfb31556b
김명진 <krocd@naver.com>
2024-01-19 03:44:01 +0900

    실버 역원소 정렬

---
+++ javaSolve/src/silver/BJ5648/Main.java
@@ -0,0 +1,43 @@
+package silver.BJ5648;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        List<String> input = new ArrayList<>();
+        int count = 0;
+        while (st.hasMoreTokens()){
+            input.add(st.nextToken());
+            count++;
+
+        }
+        while (count < N){
+            st = new StringTokenizer(br.readLine());
+            while (st.hasMoreTokens()){
+                String val = st.nextToken();
+                if(!val.equals(" ")){
+                    input.add(val);
+                    count++;
+
+                }
+            }
+        }
+        PriorityQueue<Long> sort  = new PriorityQueue<>();
+        for(String val : input){
+            StringBuilder sb = new StringBuilder(val);
+            sort.offer(Long.parseLong(sb.reverse().toString()));
+        }
+        while (!sort.isEmpty()){
+            System.out.println(sort.poll());
+        }
+    }
+}
