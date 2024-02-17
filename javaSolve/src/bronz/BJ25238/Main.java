164b0525cfda97d337f513bb8daab8f56319db77
김명진 <krocd@naver.com>
2024-02-17 10:49:55 +0900

    스트릭용 브론즈

---
+++ javaSolve/src/bronz/BJ25238/Main.java
@@ -0,0 +1,17 @@
+package bronz.BJ25238;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st  = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        double res = a - (a* (b * 0.01));
+        System.out.println((int)res >= 100 ? 0 :1 );
+    }
+}
